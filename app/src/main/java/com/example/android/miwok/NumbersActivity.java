e0096f9e8b1afddfbe2f7aca90f0bb9babaedaab
kai li <adamlee516@gmail.com>
2017-12-11 15:48:04 -0500

    listview
    listview and arrayAdapter

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -17,6 +17,10 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -24,5 +28,32 @@ public class NumbersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+
+        //create an arrayList of words
+        ArrayList<String> words = new ArrayList<>();
+        words.add("one");
+        words.add("two");
+        words.add("three");
+        words.add("four");
+        words.add("five");
+        words.add("six");
+        words.add("seven");
+        words.add("eight");
+        words.add("night");
+        words.add("ten");
+//
+//        LinearLayout numbersRootView = (LinearLayout)findViewById(R.id.numbers_rootView);
+//
+//
+//        for (int i = 0; i < words.size(); i++) {
+//            TextView wordView = new TextView(this);
+//            wordView.setText(words.get(i));
+//            numbersRootView.addView(wordView);
+//        }
+
+        //Listview and view recycling
+        ArrayAdapter<String> itemAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, words);
+        ListView listView = (ListView)findViewById(R.id.list);
+        listView.setAdapter(itemAdapter);
     }
 }
